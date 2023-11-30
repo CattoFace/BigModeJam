6b65a7f62a7505e0632ef8b9913ed76915b758a0
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2023-11-30 13:42:28 +0200

    started working on balls falling from heaven stage, added colors and some physics

---
+++ Assets/Scripts/FallingBall.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBall : MonoBehaviour
+{
+    public GameObject sphere;
+    public int numSpheres = 10;
+    public float minSize = 0.1f;
+    public float maxSize = 5f;
+    public float scaleChangeAmount;
+    Rigidbody rb;
+    public float maxVelocity = 12f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scaleChangeAmount = Random.Range(0.3f, 0.6f);
+        rb = GetComponent<Rigidbody>();
+        rb.drag = Random.Range(0.5f, 0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Get the current scale
+            Vector3 currentScale = transform.localScale;
+
+            // Calculate the new scale with the specified change amount
+            float newScaleX = Mathf.Clamp(currentScale.x + scaleChangeAmount, minSize, maxSize);
+            float newScaleY = Mathf.Clamp(currentScale.y + scaleChangeAmount, minSize, maxSize);
+            float newScaleZ = Mathf.Clamp(currentScale.z + scaleChangeAmount, minSize, maxSize);
+
+            // Set the new scale of the sphere
+            transform.localScale = new Vector3(newScaleX, newScaleY, newScaleZ);
+            scaleChangeAmount = -scaleChangeAmount;
+
+        }
+        if (transform.position.y < -3f)
+        {
+            //prep position to top of screen
+            Vector3 newPos = transform.position;
+            newPos.y = 10f;
+            newPos.z = Random.Range(-4f, 4f);
+            newPos.x = Random.Range(-14f, -6f);
+            
+            //rescale for more fun
+            // Get the current scale
+            Vector3 currentScale = transform.localScale;
+            // Calculate the new scale with the specified change amount
+            float newScaleX = Mathf.Clamp(currentScale.x + scaleChangeAmount, minSize, maxSize);
+            float newScaleY = Mathf.Clamp(currentScale.y + scaleChangeAmount, minSize, maxSize);
+            float newScaleZ = Mathf.Clamp(currentScale.z + scaleChangeAmount, minSize, maxSize);
+            // Set the new scale of the sphere
+            transform.localScale = new Vector3(newScaleX, newScaleY, newScaleZ);
+            scaleChangeAmount = -scaleChangeAmount;
+          
+            //update position to top of screen
+            transform.position = newPos;
+            
+        }
+    }
+}
