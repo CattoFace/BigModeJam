ccf508aa472258769b16ada57722054c078c67e1
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2023-12-04 22:32:59 +0200

    progress on level manager, progress on fallingspheresprefab, added invisble walls, general progress

---
+++ Assets/Scripts/FallingSpheresPrefabScript.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpheresF : MonoBehaviour
+{
+    //numspheres need to be s.t. numSpheres MOD 8 = 2, 100 is a good choice
+    public int numSpheres = 100;
+    public float minSize = 0.1f;
+    public float maxSize = 5f;
+    public float scaleChangeAmount;
+    Rigidbody rb;
+    Renderer sphereRenderer;
+    public float maxVelocity = 18f;
+    private Color winColor;
+    private int winColorIndex;
+    private Color[] colors;
+    private int counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activateQ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void activateQ()
+    {
+        winColorIndex = Random.Range(0, 7);
+        colors = new Color[] { Color.red, Color.blue, Color.green, Color.yellow, Color.black, Color.white, Color.cyan, new Color(0.5f, 0f, 0.5f) };
+        winColor = colors[winColorIndex];
+        Debug.Log(winColorIndex);
+        counter = 0;
+        SpawnSpheres();
+    }
+    void SpawnSpheres()
+    {
+        int j = 0;
+        for (int i = 0; i < numSpheres; i++)
+        {
+            Vector3 spawnLocation = new Vector3(Random.Range(-14f, -6f), 5f, Random.Range(-4, 4));
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = spawnLocation;
+            sphere.transform.SetParent(transform);
+            sphere.AddComponent<Rigidbody>();
+            rb = sphere.GetComponent<Rigidbody>();
+            rb.drag = Random.Range(0.3f, 0.75f);
+            sphereRenderer = sphere.GetComponent<Renderer>();
+            //if j is under 7 means we're still spawning 7 spheres of each color(0-7 indices)
+            if (j <= 7)
+            {
+                sphereRenderer.material.color = colors[j];
+            }
+            else
+            {
+                sphereRenderer.material.color = colors[winColorIndex];
+            }
+            sphere.transform.localScale *= Random.Range(1.1f, 1.5f);
+            sphere.AddComponent<FallingBall>();
+            sphere.layer = 7;
+            counter++;
+            if (counter == 6)
+            {
+                j++;
+                counter = 0;
+            }
+        }
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,9 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LevelManager : MonoBehaviour
 {
-    public float submitAnswer(int answer){
+    public int score;
+
+    public GameObject currQuestionPrefab;
+    private GameObject toDest;
+
+    void Start()
+    {
+
+    }
+    //update is that function that updates every frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            doIt();
+        }
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            Destroy(toDest);
+        }
+
+    }
+    void doIt()
+    {
+        toDest = Instantiate(currQuestionPrefab, new Vector3(-10f, 0, 0), Quaternion.identity);
+    }
+    public float submitAnswer(int answer)
+    {
         return 0;
     }
 }
