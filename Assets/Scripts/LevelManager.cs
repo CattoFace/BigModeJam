588a1605a0bec5cde5dc9e9c9be44a20e3a5fbbe
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2023-12-06 13:13:20 +0200

    managed to fix levelmanager s.t it loads prefabs from (newly added) Resources folder in Assets. now we need to create more levels and work on scoring system

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -9,21 +9,26 @@ public class LevelManager : MonoBehaviour
     public bool isAlive = false;
     public GameObject currQuestionPrefab;
     private GameObject toDest;
-
+    public string[] questionPrefabNames; 
     void Start()
     {
-
+        questionPrefabNames = new string[] { "SpheresFallingPrefab" };
     }
     //update is that function that updates every frame
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Space)) {
             if (!isAlive){
+                currQuestionPrefab = Resources.Load(questionPrefabNames[0]) as GameObject;
+                if(currQuestionPrefab == null) {
+                    Debug.Log("NO");
+                }
                 doIt();
                 isAlive = true;
             }
             else
             {
+                currQuestionPrefab = null;
                 Destroy(toDest);
                 isAlive = false;    
             }
