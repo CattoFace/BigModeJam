900cb225cd734e1988b422e2d52ef79738f30839
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2023-12-08 14:50:01 +0200

    working on stages

--- Assets/Scripts/ObjectsFallingScript.cs
+++ Assets/Scripts/ObjectsFallingScript.cs
@@ -8,7 +8,7 @@ public class ObjectsFallingScript : MonoBehaviour
     public float minSize = 0.1f;
     public float maxSize = 5f;
     public float scaleChangeAmount;
-    public int relativeDifficulty;
+    public float  relativeDifficulty;
     Rigidbody rb;
     Renderer objectRenderer;
     public float maxVelocity = 18f;
@@ -16,23 +16,25 @@ public class ObjectsFallingScript : MonoBehaviour
     private int winObjectIndex;
     private GameObject[] Objects;
     private int counter;
-    public GameObject bed;
+    public GameObject Bed;
     public GameObject hockeyTable;
     public GameObject box;
     public GameObject coffeM;
-    public GameObject table1;
-    public GameObject chair1;
+    public GameObject brownTable;
+    public GameObject greenChair;
     public GameObject fridge;
     public GameObject dresser;
     public GameObject toyCar;
     public GameObject sofa;
-    public GameObject table2;
+    public GameObject grayTable;
     public GameObject microwave;
-    public GameObject chair2;
+    public GameObject brownChair;
     public GameObject lamp;
-    public GameObject dbRack;
-    public GameObject washingM;
+    public GameObject plateRack;
+    public GameObject washingMachine;
     public Vector3 commonScale;
+    public int spawnDifficulty;
+    private string[] answers;
 
     
     // Start is called before the first frame update
@@ -43,24 +45,33 @@ public class ObjectsFallingScript : MonoBehaviour
 
     public void activated()
     {
-        Objects = new GameObject[] {bed, hockeyTable, box, coffeM, table1,
-                             chair1, fridge, dresser, toyCar, sofa,
-                             table2, microwave, chair2, lamp, dbRack,
-                             washingM};
-        relativeDifficulty = 0;
+        Objects = new GameObject[] {Bed, hockeyTable, box, coffeM, brownTable,
+                             greenChair, fridge, dresser, toyCar, sofa,
+                             grayTable, microwave, brownChair, lamp, plateRack,
+                             washingMachine};
+        relativeDifficulty = Random.Range(1.75f,2.9f);
+        spawnDifficulty = Random.Range(0, 3);
         winObjectIndex = Random.Range(0, 16);
         winObject = Objects[winObjectIndex];
         commonScale = new Vector3(1.3f, 1.3f, 1.3f);
+        if(winObjectIndex == 5 || winObjectIndex == 12) //case chair
+        {
+            if (nameof(winObject) == "greenChair")
+            {
+                answers = new string[] { "Green Chair", "Brown Chair" };
+            }
+            
+        }
+        answers = new string[] {nameof(winObject),nameof(Bed) };
         counter = 0;
-        numObjects = 151;
+        numObjects = 156;
         SpawnObjects();
-        Debug.Log(winObject.name);
-
+        Debug.Log("winner: " + winObject.name + " , num objects: " + numObjects +
+                ", difficulty: " + spawnDifficulty);
     }
 
     public void SpawnObjects()
     {
-        float difficulty = Random.Range(1f, 1.75f);
         GameObject tempObj;
         string objType;
         int j = 0;
@@ -116,21 +127,31 @@ public class ObjectsFallingScript : MonoBehaviour
                     tempObj.transform.localScale = commonScale;
                     break;
             }
-            //apply difficulty
-            tempObj.transform.localScale *= difficulty;
+            if (j == winObjectIndex && j != 0 && j != 1 && j != 4 && j != 6 && j != 9 && j != 10 && j != 15)
+            {
+                tempObj.transform.localScale *= (3.65f - relativeDifficulty);
+            }
+            else
+            {
+                tempObj.transform.localScale *= 1.25f;
+            }
+
             tempObj.GetComponent<FallingObject>().setType(objType);
             //end of section  
             ++counter;
-            if (counter == 7)
+            if (counter == 8 + spawnDifficulty)
             {
                 j++;
                 counter = 0;
             }
         }
-        Debug.Log(numObjects);
         return;
     }
-    
+
+    public string[] getAnswersForButtons()
+    {
+        return null;
+    }
     // Update is called once per frame
     void Update()
     {
